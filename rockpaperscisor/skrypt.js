eb5e4185e0a351771a3aa3164ccb877e37d6385a
Mrożon <alanmrozik25@gmail.com>
2022-08-24 22:32:14 +0200

    dodanie klasy dla flexu

--- rockpaperscisor/skrypt.js
+++ rockpaperscisor/skrypt.js
@@ -1,6 +1,22 @@
 let wybor = ["rock","paper","scissor"];
-let player_points=0;
-let si_points=0;
+
+class Player{
+    constructor(name,points,choice){
+        this.name=name;
+        this.points=points;
+        this.choise=choice;
+    }
+
+    changePoints(){
+        this.points++;
+    }
+
+    setChoice(w){
+        this.choice=w;
+    }
+}
+let humanplayer = new Player("player",0,0);
+let siplayer = new Player("si",0,0);
 let lock=false;
 
 document.getElementById("rock").addEventListener("click",function(){klikom(wybor[0]);});
@@ -18,48 +34,51 @@ function klikom(wybor_gracza){
         lock=true;
         document.getElementById("player").innerHTML=wybor_gracza;
         let liczba=losujLiczbe();
-        let wybor_si=wybor[liczba];
+        siplayer.setChoice(wybor[liczba]);
+        humanplayer.setChoice(wybor_gracza);
 
         animuj();
-        setTimeout(function(){zmienGrafiki("player","si",wybor_gracza,wybor_si)},1000);
-        setTimeout(function(){whoWin(wybor_gracza,wybor_si)},1000);
+        setTimeout(function(){zmienGrafiki(humanplayer.name,siplayer.name,humanplayer.choice,siplayer.choice)},1000);
+        setTimeout(function(){whoWin(humanplayer.choice,siplayer.choice)},1000);
     }
 }
 
 function whoWin(gracz,si){
     if(gracz==wybor[0]){
         if(si==wybor[1]){
-            si_points++;
-            document.getElementById("siscore").innerHTML=si_points;
+            siplayer.changePoints();
+            document.getElementById("siscore").innerHTML=siplayer.points;
         }else if(si==wybor[2]){
-            player_points++;
-            document.getElementById("playerscore").innerHTML=player_points;
+            humanplayer.changePoints();
+            document.getElementById("playerscore").innerHTML=humanplayer.points;
         }//else alert("draw");
     }else if(gracz==wybor[1]){
         if(si==wybor[2]){
-            si_points++;
-            document.getElementById("siscore").innerHTML=si_points;
+            siplayer.changePoints();
+            document.getElementById("siscore").innerHTML=siplayer.points;
         }else if(si==wybor[0]){
-            player_points++;
-            document.getElementById("playerscore").innerHTML=player_points;
+            humanplayer.changePoints();
+            document.getElementById("playerscore").innerHTML=humanplayer.points;
         }//else alert("draw");
     }else if(gracz==wybor[2]){
         if(si==wybor[0]){
-            si_points++;
-            document.getElementById("siscore").innerHTML=si_points;
+            siplayer.changePoints();
+            document.getElementById("siscore").innerHTML=siplayer.points;
         }else if(si==wybor[1]){
-            player_points++;
-            document.getElementById("playerscore").innerHTML=player_points;
+            humanplayer.changePoints();
+            document.getElementById("playerscore").innerHTML=humanplayer.points;
         }//else alert("draw");
     }
     lock=false;
 }
 
+//podmienia grafiki na odpowiedni wybor "broni"
 function zmienGrafiki(gracz,si,wybor1,wybor2){
     document.getElementById(gracz).innerHTML='<img src="'+wybor1+'.png">';
     document.getElementById(si).innerHTML='<img src="'+wybor2+'.png" class="simage">';
 }
 
+//animacja poprzez dodanie klasy lub jej ponowna aktywacja
 function animuj(){
     document.getElementById("player").innerHTML='<img src="rock.png">';
     document.getElementById("si").innerHTML='<img src="rock.png" class="simage">';
@@ -71,6 +90,7 @@ function animuj(){
     resetAnimation();
 }
 
+//resetuje proces animacji
 function resetAnimation(){
     let p=document.getElementById('player');
     p.style.animation='none';
